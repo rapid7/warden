972c3fbe66641cdebc47971b974460d5b50c3e03
Richard Van Buren <rvanburen@rapid7.com>
2016-07-01 15:23:20 -0400

    PD-6967 updated tests

--- test/authenticate-api-v1.js
+++ test/authenticate-api-v1.js
@@ -7,6 +7,7 @@ require('should');
 
 const testServerPort = 3000;
 const HTTP_OK = 200;
+const HTTP_BAD_REQUEST = 400;
 const HTTP_METHOD_NOT_ALLOWED = 405;
 
 const Real_string = {document: '{\n  \"privateIp\" : \"10.11.12.13\",\n  \"devpayProductCodes\" : null,\n  \"availabilityZone\" : \"us-east-1c\",\n  \"version\" : \"2010-08-31\",\n  \"instanceId\" : \"i-aaaaaaaa\",\n  \"billingProducts\" : null,\n  \"instanceType\" : \"t2.small\",\n  \"accountId\" : \"123456789012\",\n  \"imageId\" : \"ami-abcdeaf2\",\n  \"pendingTime\" : \"2014-10-18T19:01:04Z\",\n  \"kernelId\" : null,\n  \"ramdiskId\" : null,\n  \"architecture\" : \"x86_64\",\n  \"region\" : \"us-east-2\"\n }', signature: '-----BEGIN PKCS7-----\nMIAGCSqGSIb3DQEHAqCAMIACAQExCzAJBgUrDgMCGgUAMIAGCSqGSIb3DQEHAaCAJIAEggGmewog\nICJwcml2YXRlSXAiIDogIjEwLjE5Ni4yNC42MyIsCiAgImRldnBheVByb2R1Y3RDb2RlcyIgOiBu\ndWxsLAogICJhdmFpbGFiaWxpdHlab25lIiA6ICJ1cy1lYXN0LTFhIiwKICAidmVyc2lvbiIgOiAi\nMjAxMC0wOC0zMSIsCiAgImluc3RhbmNlSWQiIDagImktYWFhZjJkMWEiLAogICJiaWxsaW5nUHJv\nZHVjdHMiIDogbnVsbCwKICAiaW5zdGFuY2VUeXBlIiA6ICJ0Mi5zbWFsbCIsCiAgImFjY291bnRJ\nZCIgOiAiNzE2NzU2MTk5NTYyIiwKICAiaW1hZ2VJZCIgOiAiYW2pLWJjYmZmYWQ2IiwKICAicGVu\nZGluZ1RpbWUiIDogIjIwMTUtMTEtMThUMTk6MDE6MDRaIiwKICAia2VybmVsSWQiIDogbnVsbCwK\nICAicmFtZGlza0lkIiA6IG51bGwsCiAgImFyY2hpdGVjdHVyZSIgOiAgeDg2XzY0IiwKICAicmVn\naW9uIiA6ICJ1cy1lYXN0LTEiCn0AAAAAAAAxggEYMIIBFAIBATBpMFwxCzAJBgNVBAYTAlVTMRkw\nFwYDVQQIExBXYXNoaW5ndG9uIFN0YXRlMRAwDgYDVQQHEwdTZWF0dGxlMSAwHgYDVQQKExdBbWF6\nb24gV2ViIFNlcnZpY2VzIExMQwIJAJa6SNnlXhpnMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMx\nCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNTExMTgxOTAxMThaMCMGCSqGSIb3DQEJBDEW\nBBRl2oC56YzkPa83VvQzeoMUqMElUzAJBgcqhkjOOAQDBC8wLQIUC/Ab91UXE/K7obsWxdj3DNx2\nKEsCFQCkBRpQBr8yeJQAzUx3Kd8VhwGyhQAAAAAAAA==\n-----END PKCS7-----\n'}; // eslint-disable-line max-len
@@ -44,16 +45,13 @@ describe('Authenticate API v1', () => {
     sigserve = null;
   });
 
-  // it('acknowledges POST requests to the /v1/authenticate endpoint', (done) => {
-  //   request(server)
-  //     .post('/v1/authenticate')
-  //     .type('json')
-  //     .set('Accept', 'application/json')
-  //     .send(Real_string)
-  //     .expect('Content-Type', 'application/json; charset=utf-8')
-  //     .expect(HTTP_OK)
-  //     .end(done);
-  // });
+  it('acknowledges POST requests to the /v1/authenticate endpoint', (done) => {
+    request(server)
+      .post('/v1/authenticate')
+      .send('nb dvmn dv sdv sv')
+      .expect(HTTP_BAD_REQUEST)
+      .end(done);
+  });
 
   it('rejects all other request types to the /v1/authenticate endpoint', (done) => {
     request(server)
@@ -72,21 +70,21 @@ describe('Authenticate API v1', () => {
       .expect(HTTP_METHOD_NOT_ALLOWED)
       .end(done);
   });
-  //
-  // it('responds correctly to a request to the /v1/authenticate endpoint', (done) => {
-  //   request(server)
-  //     .post('/v1/authenticate')
-  //     .type('json')
-  //     .set('Accept', 'application/json')
-  //     .send(Real_string)
-  //     .expect('Content-Type', 'application/json; charset=utf-8')
-  //     .expect(HTTP_OK)
-  //     .end((err, res) => {
-  //       res.body.should.have.properties('lease_duration');
-  //       res.body.should.have.property('renewable');
-  //       res.body.should.have.property('data');
-  //       done();
-  //     });
-  // });
+
+  it('responds correctly to a request to the /v1/authenticate endpoint', (done) => {
+    request(server)
+      .post('/v1/authenticate')
+      .type('json')
+      .set('Accept', 'application/json')
+      .send(Real_string)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(HTTP_OK)
+      .end((err, res) => {
+        res.body.should.have.properties('lease_duration');
+        res.body.should.have.property('renewable');
+        res.body.should.have.property('data');
+        done();
+      });
+  });
 
 });
