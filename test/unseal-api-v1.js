76309e9bf460d97f6b72a32ccad3c0a061faf0a3
Richard Van Buren <rvanburen@rapid7.com>
2016-10-04 14:21:24 -0400

    PD-8244 removed redundant test

--- test/unseal-api-v1.js
+++ test/unseal-api-v1.js
@@ -8,9 +8,15 @@ const request = require('supertest');
 const expect = require('expect')
 const should = require('should');
 
+const unseal = require('../lib/control/v1/unseal');
+
 const testServerPort = 3000;
 const HTTP_OK = 200;
 const HTTP_METHOD_NOT_ALLOWED = 405;
+
+const good = {token: '12345678-abcd-1234-!@#$-123456789abc'};
+const res = {status() {return this}, json(any) {return any}};
+const next = function() {return true};
 
 const endpoints = {
   unseal: '/v1/unseal'
@@ -24,7 +30,7 @@ const endpoints = {
 const makeServer = () => {
   const app = require('express')();
 
-  require('../lib/control/v1/unseal').attach(app);
+  unseal.attach(app);
   return app.listen(testServerPort);
 };
 
@@ -44,17 +50,10 @@ describe('unseal API v1', () => {
     server.close(done);
   });
 
-  it('responds correctly to a request to the /unseal endpoint', (done) => {
-    request(server)
-      .post('/v1/unseal')
-      .expect(HTTP_OK)
-      .end(done);
-  });
-
   it('Vault:token is set after call', (done) => {
     request(server)
       .post('/v1/unseal')
-      .send('12345678-abcd-1234-!@#$-123456789acb')
+      .send(good)
       .expect(HTTP_OK)
       .end(function(){
         expect(global.Config.get('vault:token')).toExist();
